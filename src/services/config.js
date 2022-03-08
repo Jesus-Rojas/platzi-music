7a7dad3c0625f955de30dfe9fc787c6f1fab303d
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-08 01:20:35 -0500

    fix build, problems install dependencies in vue-cli with webpack. The page oficial is working.

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,6 @@
 const configService = {
-  apiUrl: 'https://platzi-music-api.now.sh'
+  apiUrl: 'https://platzi-music-api.herokuapp.com'
+  // apiUrl: 'https://platzi-music-api.now.sh'
 }
 
 export default configService
